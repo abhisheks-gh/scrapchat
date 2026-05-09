788152fb5d40c8c7989a9948620ab1b3b7576e30
HAPPY Happy <randeeps2000@gmail.com>
2026-05-10 02:05:58 +0530

    add login, chat pages

--- frontend/scrapchat/src/main.jsx
+++ frontend/scrapchat/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router"
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from "./pages/Login.jsx"
+import Chat from "./pages/Chat.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/Chat" element={<Chat/>}/>
+      </Routes>
+  </BrowserRouter>
 )
---
+++ frontend/scrapchat/src/pages/Chat.jsx
@@ -0,0 +1,7 @@
+export default function Chat(){
+    return (
+        <>
+            <h1>This is the chat page</h1>
+        </>
+    )
+};
