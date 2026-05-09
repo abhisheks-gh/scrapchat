fcbde0533a0de9599d566ece0b94330d04ad2385
abhisheks-gh <abhishek8598kumar@gmail.com>
2026-05-10 00:58:57 +0530

    Initial commit -- project setup

---
+++ src/main/java/com/example/scrapchat/ScrapchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrapchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scrapchat/ScrapchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrapchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
